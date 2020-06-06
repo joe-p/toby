a3e0df4db6a76bc700c376c3f6e7b25cf9eb94ef
Joe Polny <joepolny@gmail.com>
2020-06-06 19:08:26 -0400

    datetime support

--- lib/toby/datetime.rb
+++ lib/toby/datetime.rb
@@ -1,40 +1,66 @@
 # frozen_string_literal: true
 
 module Toby
-  module OffsetDateTimeParser
-    def value
-      skeleton = captures[:datetime_skeleton].first
-      year, mon, day, hour, min, sec, sec_frac = skeleton.value
-      offset = captures[:date_offset].first || '+00:00'
-      sec = "#{sec}.#{sec_frac}".to_f
-
-      Time.new(year, mon, day, hour, min, sec, offset.to_s)
+  class OffsetDateTime < Time
+    def to_s
+      strftime('%Y-%m-%dT%H:%M:%S%z')
     end
   end
 
-  module LocalDateTimeParser
-    def value
-      year, mon, day = captures[:date_skeleton].first.value
-      hour, min, sec, sec_frac = captures[:time_skeleton].first.value
-      usec = sec_frac.to_s.ljust(6, '0')
-
-      Time.local(year, mon, day, hour, min, sec, usec)
+  class LocalDateTime < Time 
+    def to_s
+      strftime('%Y-%m-%dT%H:%M:%S')
     end
   end
 
-  module LocalDateParser
-    def value
-      year, mon, day = captures[:date_skeleton].first.value
-      Time.local(year, mon, day)
+  class LocalDate < Time
+    def to_s
+      strftime('%Y-%m-%d')
     end
   end
 
-  module LocalTimeParser
-    def value
-      hour, min, sec, sec_frac = captures[:time_skeleton].first.value
-      usec = sec_frac.to_s.ljust(6, '0')
-
-      Time.at(3600 * hour.to_i + 60 * min.to_i + sec.to_i, usec.to_i)
+  class LocalTime < Time
+    def to_s
+      strftime('%Y-%m-%dT%H:%M:%S')
     end
   end
+
+  module Match
+    module OffsetDateTime
+      def value
+        skeleton = captures[:datetime_skeleton].first
+        year, mon, day, hour, min, sec, sec_frac = skeleton.value
+        offset = captures[:date_offset].first || '+00:00'
+        sec = "#{sec}.#{sec_frac}".to_f
+
+        Toby::OffsetDateTime.new(year, mon, day, hour, min, sec, offset.to_s)
+      end
+    end
+
+    module LocalDateTime
+      def value
+        year, mon, day = captures[:date_skeleton].first.value
+        hour, min, sec, sec_frac = captures[:time_skeleton].first.value
+        usec = sec_frac.to_s.ljust(6, '0')
+
+        Toby::LocalDateTime.local(year, mon, day, hour, min, sec, usec)
+      end
+    end
+
+    module LocalDate
+      def value
+        year, mon, day = captures[:date_skeleton].first.value
+        Toby::LocalDate.local(year, mon, day)
+      end
+    end
+
+    module LocalTime
+      def value
+        hour, min, sec, sec_frac = captures[:time_skeleton].first.value
+        usec = sec_frac.to_s.ljust(6, '0')
+
+        Toby::LocalTime.at(3600 * hour.to_i + 60 * min.to_i + sec.to_i, usec.to_i)
+      end
+    end
   end
+end
